8262ca0d4f6c02ec8a313fd9e587324d8d8274c1
Mitko Nikolov <dnnmitko@gmail.com>
2017-07-10 23:40:31 +0300

    Added main.cpp and Constants.h
    Added a main.cpp with the main loop and Constants.h with screen sizes.

---
+++ Constants.h
@@ -0,0 +1,11 @@
+#ifndef CONSTANTS_H_
+#define CONSTANTS_H_
+
+
+
+const int SCREEN_WIDTH = 1280;
+const int SCREEN_HEIGHT = 720;
+
+
+
+#endif /* CONSTANTS_H_ */
---
+++ main.cpp
@@ -0,0 +1,20 @@
+#include "GameManager.h"
+
+int main(int argc, char* args[] )
+{
+	GameManager game;
+
+	SDL_Event e;
+
+	while( !game.getQuit() )
+	{
+		while( SDL_PollEvent( &e ) != 0 )
+		{
+			game.eventHandler( e );
+		}
+
+		game.render();
+	}
+
+	return 0;
+}
