5b9bbce82c9ce5e421eb364ce08a209bac17675b
Mitko Nikolov <dnnmitko@gmail.com>
2017-07-10 23:43:21 +0300

    Fixed formatting issue

--- main.cpp
+++ main.cpp
@@ -1,16 +1,16 @@
 #include "GameManager.h"
 
-int main(int argc, char* args[] )
+int main(int argc, char* args[])
 {
 	GameManager game;
 
 	SDL_Event e;
 
-	while( !game.getQuit() )
+	while(!game.getQuit())
 	{
-		while( SDL_PollEvent( &e ) != 0 )
+		while(SDL_PollEvent(&e) != 0)
 		{
-			game.eventHandler( e );
+			game.eventHandler(e);
 		}
 
 		game.render();
