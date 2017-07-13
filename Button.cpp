d339588325b032ef1770437f3797f44b520b05b8
Mitko Nikolov <dnnmitko@gmail.com>
2017-07-13 12:24:56 +0300

    Added Button class

--- Button.cpp
+++ Button.cpp
@@ -1,36 +1,118 @@
-/*
- * Button.cpp
- *
- *  Created on: Jul 10, 2017
- *      Author: Ivan
- */
-
 #include "Button.h"
 
+SDL_Texture* Button::m_TextureButton = NULL;
 
-Button::Button()
+Button::Button() : TextField()
 {
+	m_TextureTextPressed = NULL;
 
+	m_bIsPressed = false;
+}
+
+Button::Button(SDL_Renderer* newRenderer) : TextField(newRenderer)
+{
+	m_TextureTextPressed = NULL;
+
+	m_bIsPressed = false;
+
+	if(m_TextureButton == NULL)
+	{
+		m_TextureButton = IMG_LoadTexture(m_Renderer, g_ButtonSprite);
+		if(m_TextureButton == NULL)
+			std::cerr << "Failed to load TextureBackground! SDL Error: " << IMG_GetError() << std::endl;
+	}
 }
 
 Button::~Button()
 {
+	SDL_DestroyTexture(m_TextureTextPressed);
+	m_TextureTextPressed = NULL;
 
+	SDL_DestroyTexture(m_TextureButton);
+	m_TextureButton = NULL;
+}
+
+void Button::RenderForce()
+{
+	if(m_Renderer == NULL || m_TextureText == NULL || m_TextureTextPressed == NULL || m_TextureButton == NULL)
+		return;
+
+	SDL_Rect tempRect;
+	tempRect.w = g_ButtonSpriteWidth;
+	tempRect.h = g_ButtonSpriteHeight / 2;
+	tempRect.x = 0;
+	if(m_bIsPressed)
+		tempRect.y = g_ButtonSpriteHeight / 2;
+	else
+		tempRect.y = 0;
+
+	SDL_RenderCopy(m_Renderer, m_TextureButton, &tempRect, &m_FieldRect);
+
+	m_TextRect.x = m_FieldRect.x + (m_FieldRect.w - m_TextRect.w) / 2;
+	m_TextRect.y = m_FieldRect.y + (m_FieldRect.h - m_TextRect.h) / 2;
+
+	if(m_bIsPressed)
+		SDL_RenderCopy(m_Renderer, m_TextureTextPressed, NULL, &m_TextRect);
+	else
+		SDL_RenderCopy(m_Renderer, m_TextureText, NULL, &m_TextRect);
+}
+
+void Button::SetText(std::string newText, TTF_Font* font, SDL_Color color)
+{
+	Label::SetText(newText, font, color);
+
+	SDL_DestroyTexture(m_TextureTextPressed);
+
+	SDL_Surface* tempSurface = TTF_RenderText_Blended(font, newText.c_str(), SDL_Color{0xFF, 0xFF, 0xFF, 0xFF});
+
+	m_TextureTextPressed = SDL_CreateTextureFromSurface(m_Renderer, tempSurface);
+
+	if( m_TextureTextPressed == NULL )
+	{
+		std::cerr << "Unable to create texture from rendered text \"" << newText << "\"! SDL Error: " <<  SDL_GetError() << "\n";
+	}
+
+	SDL_FreeSurface(tempSurface);
+	tempSurface = NULL;
+}
+
+bool Button::IsIn(int x, int y) const
+{
+	if(x < m_FieldRect.x || x > (m_FieldRect.x + m_FieldRect.w))
+		return false;
+	else if(y < m_FieldRect.y || y > (m_FieldRect.y + m_FieldRect.h))
+		return false;
+
+	return true;
 }
 
 void Button::Press()
 {
-	// TODO code here
+	m_bIsPressed = true;
+	m_bHasChanged =  true;
 }
 
 void Button::Release()
 {
-	// TODO code here
+	m_bIsPressed = false;
+	m_bHasChanged =  true;
 }
 
-bool Button::IsPressed()
+bool Button::IsPressed() const
 {
-	bool isPressed = false;
-	// TODO code here
-	return isPressed;
+	return m_bIsPressed;
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- Button.h
+++ Button.h
@@ -1,35 +1,32 @@
-/*
- * Button.h
- *
- *  Created on: Jul 10, 2017
- *      Author: Ivan
- */
-
 #ifndef BUTTON_H_
 #define BUTTON_H_
 
+#include <SDL2/SDL_image.h>
 #include "TextField.h"
+#include "Constants.h"
 
-class Button : public TextField
+class Button: public TextField
 {
-
 public:
-
 	Button();
+	Button(SDL_Renderer*);
 	virtual ~Button();
-
 public:
+	void RenderForce();
+
+	void SetText(std::string, TTF_Font*, SDL_Color);
+
+	bool IsIn(int, int) const;
 
 	void Press();
 	void Release();
-	bool IsPressed();
-
+	bool IsPressed() const;
 private:
+	bool m_bIsPressed;
 
+	SDL_Texture* m_TextureTextPressed;
 
-private:
-
-	bool m_bPressed = false;
+	static SDL_Texture* m_TextureButton;
 };
 
 #endif /* BUTTON_H_ */
