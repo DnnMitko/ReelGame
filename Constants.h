cbd5bd80c1eb255a94e84d41a4bf9e17c5669941
Mitko Nikolov <dnnmitko@gmail.com>
2017-07-13 11:38:13 +0300

    Added info to Constants
    Added button sprite location and sizes

--- Constants.h
+++ Constants.h
@@ -6,6 +6,9 @@
 const int g_ScreenWidth = 1280;
 const int g_ScreenHeight = 720;
 
+const char g_ButtonSprite[] = "images/ButtonSprite.png";
+const int g_ButtonSpriteWidth = 296;
+const int g_ButtonSpriteHeight = 174;
 
 
 #endif /* CONSTANTS_H_ */
