2f6d3824025aa1a505a1cc6e4361af3b63273491
Mitko Nikolov <dnnmitko@gmail.com>
2017-07-13 14:17:34 +0300

    Added Outro sizes to Constants.h

--- Constants.h
+++ Constants.h
@@ -8,6 +8,9 @@ const int g_ScreenHeight = 720;
 
 const char g_LabelFont[] = "fonts/Roboto-Bold.ttf";
 
+const int g_OutroWidth = 700;
+const int g_OutroHeight = 360;
+
 const char g_ButtonSprite[] = "images/ButtonSprite.png";
 const int g_ButtonSpriteWidth = 296;
 const int g_ButtonSpriteHeight = 174;
