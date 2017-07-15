a9c84468e9a1dfee4f486a05df2043a75eb5adfe
Mitko Nikolov <dnnmitko@gmail.com>
2017-07-15 15:11:32 +0300

    Added initial variant of class Game

---
+++ Game.cpp
@@ -0,0 +1,20 @@
+/*
+ * Game.cpp
+ *
+ *  Created on: 15.07.2017 ã.
+ *      Author: Mitko
+ */
+
+#include "Game.h"
+
+Game::Game()
+{
+	// TODO Auto-generated constructor stub
+
+}
+
+Game::~Game()
+{
+	// TODO Auto-generated destructor stub
+}
+
---
+++ Game.h
@@ -0,0 +1,46 @@
+#ifndef GAME_H_
+#define GAME_H_
+
+#include "State.h"
+#include "Button.h"
+
+class Game : public State
+{
+public:
+	Game();
+	Game(SDL_Renderer*);
+	virtual ~Game();
+public:
+	void Render(bool = true);
+	void EventHandler(SDL_Event&);
+	void SetCredits(unsigned int);
+private:
+	unsigned int m_uiCurrentCredits;
+
+	Button* m_ButtonPayTable;
+
+	Label* m_LabelBet;
+	TextField* m_TextFieldBet;
+	Button* m_ButtonBetMinus;
+	Button* m_ButtonBetPlus;
+
+	Label* m_LabelLines;
+	TextField* m_TextFieldLines;
+	Button* m_ButtonLinesMinus;
+	Button* m_ButtonLinesPlus;
+
+	Button* m_ButtonMaxLines;
+
+	Label* m_LabelCurCredits;
+	TextField* m_TextFieldCurCredits;
+
+	Label* m_LabelTotalBet;
+	TextField* m_TextFieldTotalBet;
+
+	Label* m_LabelPaid;
+	TextField* m_TextFieldPaid;
+
+	Button* m_ButtonPlay;
+};
+
+#endif /* GAME_H_ */
