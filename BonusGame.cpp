20275f4623474ff4348c4550a402171ffffc2a05
Mitko Nikolov <dnnmitko@gmail.com>
2017-07-18 11:18:56 +0300

    Implemented rest of BonusGame class

--- BonusGame.cpp
+++ BonusGame.cpp
@@ -33,7 +33,23 @@ BonusGame::BonusGame(SDL_Renderer* newRenderer) : State(newRenderer)
 	m_TextFieldCredits->SetY(m_iY + (g_BonusHeight - m_TextFieldCredits->GetHeight()) / 2 + g_BonusCreditOffsetY);
 	m_TextFieldCredits->SetText("Choose your treasure!", m_FontCredits, SDL_Color{0xFF, 0xFF, 0xFF, 0xFF});
 
+	m_Chest1 = new Chest(m_Renderer);
+	m_Chest1->SetX(45 + m_iX);
+	m_Chest1->SetY(94 + m_iY);
+
+	m_Chest2 = new Chest(m_Renderer);
+	m_Chest2->SetX(242 + m_iX);
+	m_Chest2->SetY(94 + m_iY);
+
+	m_Chest3 = new Chest(m_Renderer);
+	m_Chest3->SetX(439 + m_iX);
+	m_Chest3->SetY(94 + m_iY);
+
 	m_uiCredits = 0;
+
+	m_uiTimer = 0;
+
+	m_bHasChosen = false;
 }
 
 BonusGame::~BonusGame()
@@ -43,6 +59,10 @@ BonusGame::~BonusGame()
 	delete m_LabelTitleSign;
 	delete m_TextFieldCredits;
 
+	delete m_Chest1;
+	delete m_Chest2;
+	delete m_Chest3;
+
 	TTF_CloseFont(m_FontCredits);
 	TTF_CloseFont(m_FontTitle);
 
@@ -51,16 +71,51 @@ BonusGame::~BonusGame()
 
 void BonusGame::EventHandler(SDL_Event& e)
 {
+	if(e.type == SDL_MOUSEBUTTONDOWN && !m_bHasChosen)
+	{
+		int x, y;
+		SDL_GetMouseState(&x, &y);
 
+		if(m_Chest1->IsIn(x, y))
+		{
+			m_Chest1->Open();
+			m_bHasChosen = true;
+
+			m_uiCredits += rand() % 28001 + 2000;
+			UpdateCredits();
+
+			m_uiTimer = SDL_GetTicks();
+		}
+		else if(m_Chest2->IsIn(x, y))
+		{
+			m_Chest2->Open();
+			m_bHasChosen = true;
+
+			m_uiCredits += rand() % 28001 + 2000;
+			UpdateCredits();
+
+			m_uiTimer = SDL_GetTicks();
+		}
+		else if(m_Chest3->IsIn(x, y))
+		{
+			m_Chest3->Open();
+			m_bHasChosen = true;
+
+			m_uiCredits += rand() % 28001 + 2000;
+			UpdateCredits();
+
+			m_uiTimer = SDL_GetTicks();
+		}
+	}
 }
 
 void BonusGame::Render(bool UpdateOnly)
 {
+	if(m_Renderer == NULL || m_TextureBackground == NULL || m_FontCredits == NULL || m_FontTitle == NULL)
+		return;
+
 	if(!UpdateOnly)
 	{
-		if(m_Renderer == NULL || m_TextureBackground == NULL || m_FontCredits == NULL || m_FontTitle == NULL)
-			return;
-
 		SDL_Rect tempRect;
 		tempRect.h = g_BonusHeight;
 		tempRect.w = g_BonusWidth;
@@ -71,19 +126,28 @@ void BonusGame::Render(bool UpdateOnly)
 	}
 
 	m_LabelTitleSign->Render(UpdateOnly);
+
+	m_Chest1->Render(UpdateOnly);
+	m_Chest2->Render(UpdateOnly);
+	m_Chest3->Render(UpdateOnly);
+
 	m_TextFieldCredits->Render(UpdateOnly);
+
+	if(m_bHasChosen)
+	{
+		if(SDL_GetTicks() - m_uiTimer >= 3000)
+			m_bSwitch = true;
+	}
 }
 
 void BonusGame::SetCredits(unsigned int credits)
-{//TODO
-	/*m_uiCredits = credits;
-	std::string strCredits;
-	std::stringstream ss;
+{
+	m_uiCredits = 10 * credits;
+}
 
-	ss << "YOU WON: " << m_uiCredits;
-	strCredits = ss.str();
-
-	m_TextFieldCredits->SetText(strCredits, m_FontCredits, SDL_Color{0xFF, 0xFF, 0xFF, 0xFF});*/
+unsigned int BonusGame::GetCredits() const
+{
+	return m_uiCredits;
 }
 
 void BonusGame::NullAll()
@@ -93,11 +157,52 @@ void BonusGame::NullAll()
 	m_LabelTitleSign = NULL;
 	m_TextFieldCredits = NULL;
 
+	m_Chest1 = NULL;
+	m_Chest2 = NULL;
+	m_Chest3 = NULL;
+
 	m_FontCredits = NULL;
 	m_FontTitle = NULL;
 
 	m_uiCredits = 0;
 
+	m_uiTimer = 0;
+
+	m_bHasChosen = false;
+
 	m_iX = 0;
 	m_iY = 0;
 }
+
+void BonusGame::UpdateCredits()
+{
+	std::string strCredits;
+	std::stringstream ss;
+
+	ss << "YOU WON: " << m_uiCredits;
+	strCredits = ss.str();
+
+	m_TextFieldCredits->SetText(strCredits, m_FontCredits, SDL_Color{0xFF, 0xFF, 0xFF, 0xFF});
+}
+
+void BonusGame::ResetGame()
+{
+	m_Chest1->Close();
+	m_Chest2->Close();
+	m_Chest3->Close();
+
+	m_bHasChosen = false;
+
+	m_TextFieldCredits->SetText("Choose your treasure!", m_FontCredits, SDL_Color{0xFF, 0xFF, 0xFF, 0xFF});
+}
+
+
+
+
+
+
+
+
+
+
+
--- BonusGame.h
+++ BonusGame.h
@@ -5,9 +5,11 @@
 #include <SDL2/SDL_ttf.h>
 #include <string>
 #include <sstream>
+#include <cstdlib>
 #include "State.h"
 #include "Button.h"
 #include "Constants.h"
+#include "Chest.h"
 
 class BonusGame : public State
 {
@@ -19,17 +21,30 @@ public:
 	void EventHandler(SDL_Event&);
 	void Render(bool = true);
 	void SetCredits(unsigned int);
+	unsigned int GetCredits() const;
+	void ResetGame();
 private:
 	void NullAll();
+
+	void UpdateCredits();
 private:
 	SDL_Texture* m_TextureBackground;
+
 	Label* m_LabelTitleSign;
 	TextField* m_TextFieldCredits;
+
+	Chest* m_Chest1;
+	Chest* m_Chest2;
+	Chest* m_Chest3;
 
 	TTF_Font* m_FontCredits;
 	TTF_Font* m_FontTitle;
 
 	unsigned int m_uiCredits;
+
+	Uint32 m_uiTimer;
+
+	bool m_bHasChosen;
 
 	int m_iX;
 	int m_iY;
