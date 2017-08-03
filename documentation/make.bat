0542b62a3d7fb171b77409f7e5ae9700a76b0621
Mitko Nikolov <dnnmitko@gmail.com>
2017-08-03 22:21:02 +0300

    Added make files for windows and linux.

---
+++ documentation/make.bat
@@ -0,0 +1,2 @@
+g++ -o reel.exe BonusGame.cpp Button.cpp Chest.cpp Constants.h Figure.cpp Game.cpp GameManager.cpp GamePanel.cpp Info.cpp Intro.cpp Label.cpp Lines.cpp main.cpp Outro.cpp pugixml.cpp Reel.cpp Slot.cpp State.cpp TextField.cpp Win.cpp -IC:\MinGW\include\SDL2 -LC:\MinGW\lib -lmingw32 -lSDL2main -lSDL2 -lSDL2_image -lSDL2_ttf -lSDL2_mixer -Wall
+
